20876928750198abe3474a3565c68a97530d8dc8
Luca3317 <lweist3317@gmail.com>
2024-01-25 11:10:06 +0100

    Backup 2

--- TMPEffects/Scripts/Extensions/Extensions.cs
+++ TMPEffects/Scripts/Extensions/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPEffects.Tags;
@@ -25,6 +26,31 @@ namespace TMPEffects.Extensions
                 }
             }
             else tags.AddRange(src);
+        }
+
+        public static int BinarySearchIndexOf<T>(this IList<T> list, T value, IComparer<T> comparer = null)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+
+            comparer = comparer ?? Comparer<T>.Default;
+
+            int lower = 0;
+            int upper = list.Count - 1;
+
+            while (lower <= upper)
+            {
+                int middle = lower + (upper - lower) / 2;
+                int comparisonResult = comparer.Compare(value, list[middle]);
+                if (comparisonResult == 0)
+                    return middle;
+                else if (comparisonResult < 0)
+                    upper = middle - 1;
+                else
+                    lower = middle + 1;
+            }
+
+            return ~lower;
         }
     }
 }
